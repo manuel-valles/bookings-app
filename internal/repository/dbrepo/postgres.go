07ce9b3d27989fedd21ae16873f3de404c51e921
Manuel Valles <manuel.valles@beamery.com>
2023-12-06 22:29:16 +0000

    Search availability by room ID and for all rooms

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -7,9 +7,11 @@ import (
 	"github.com/manuel-valles/bookings-app.git/internal/models"
 )
 
+const maxQueryLife = 3 * time.Second
+
 func (pr *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	// Make sure that DB transaction doesn't stay open during all the maxDbLifetime
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), maxQueryLife)
 	defer cancel()
 
 	var newID int
@@ -36,7 +38,7 @@ func (pr *postgresDBRepo) InsertReservation(res models.Reservation) (int, error)
 }
 
 func (pr *postgresDBRepo) InsertRoomRestriction(rr models.RoomRestriction) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), maxQueryLife)
 	defer cancel()
 
 	query := `INSERT INTO room_restrictions (start_date, end_date, room_id, reservation_id, restriction_id, created_at, updated_at) 
@@ -55,4 +57,60 @@ func (pr *postgresDBRepo) InsertRoomRestriction(rr models.RoomRestriction) error
 		return err
 	}
 	return nil
+}
+
+func (pr *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), maxQueryLife)
+	defer cancel()
+
+	var numRows int
+
+	query := `SELECT COUNT(id)
+			  FROM room_restrictions
+			  WHERE room_id = $1 AND $2 < end_date AND $3 > start_date;`
+
+	row := pr.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (pr *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), maxQueryLife)
+	defer cancel()
+
+	var rooms []models.Room
+
+	query := `SELECT r.id, r.room_name
+			  FROM rooms r
+			  WHERE r.id NOT IN (SELECT room_id FROM room_restrictions rr WHERE $1 < rr.end_date AND $2 > rr.start_date);`
+
+	rows, err := pr.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err := rows.Scan(
+			&room.ID,
+			&room.RoomName,
+		)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,14 @@
 package repository
 
-import "github.com/manuel-valles/bookings-app.git/internal/models"
+import (
+	"time"
+
+	"github.com/manuel-valles/bookings-app.git/internal/models"
+)
 
 type DatabaseRepo interface {
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(rr models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
